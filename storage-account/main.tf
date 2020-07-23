c6512b821a33732f40c78c6e12ffc82b54c6c0e6
Matt Boyd <mjb141@gmail.com>
2020-07-23 19:03:42 +0100

    Upped provider version to stop cloud shell error

--- storage-account/main.tf
+++ storage-account/main.tf
@@ -5,10 +5,11 @@ Script used within the module: "Demo: Creating a Storage Acount with Terraform"
 It assumes that the resource group used for the storage account already exists. If you want to use a script that creates the resource group
 as well as the please use the ./storage-account-with-resource-group folder 
 
+
 */
 
 provider "azurerm" {
-  version = "2.0.0"
+  version = ">2.0.0"
   features {}
 }
 
